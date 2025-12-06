d04d29012d4081c4c28c6ed3cf3c7932ddc47caf
Mykyta <nikita2001395@gmail.com>
2025-12-06 23:04:09 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { lazy } from "react";
+import { Route, Routes } from "react-router";
+import { RootLayout } from "./components/RootLayout";
+
+const LoginPage = lazy(() => import("./pages/Login"));
+const UsersPage = lazy(() => import("./pages/Users"));
+const DevicesPage = lazy(() => import("./pages/Devices"));
+const DeviceDetailsPage = lazy(() => import("./pages/DeviceDetails"));
+
 function App() {
-  return <>Hello</>;
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/" element={<RootLayout />}>
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/devices" element={<DevicesPage />} />
+        <Route path="/devices/:deviceId" element={<DeviceDetailsPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/DeviceDetails/DeviceDetailsPage.tsx
@@ -0,0 +1,3 @@
+export default function DeviceDetailsPage() {
+  return <div>Device Details</div>;
+}
