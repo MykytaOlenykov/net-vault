743b1968f33a0546e36ef3d3da2bba42d79bce0f
Mykyta <nikita2001395@gmail.com>
2025-12-06 23:20:51 +0200

    add components

---
+++ src/components/BackupPreview/index.ts
@@ -0,0 +1,1 @@
+export { BackupPreview } from "./BackupPreview";
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { MantineProvider } from "@mantine/core";
+
+import App from "./App.tsx";
+import { theme } from "./shared/theme/theme.ts";
+
+import "@mantine/core/styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <MantineProvider theme={theme}>
+        <App />
+      </MantineProvider>
     </BrowserRouter>
   </StrictMode>,
 );
