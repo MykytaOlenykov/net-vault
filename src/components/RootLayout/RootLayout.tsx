a59a07366c300a3040cf05517bd238eef9e0f7d8
Mykyta <nikita2001395@gmail.com>
2025-12-07 14:16:46 +0200

    update root layout

--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -1,10 +1,34 @@
+import { AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import { Suspense } from "react";
 import { Outlet } from "react-router";
 
 export const RootLayout = () => {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Outlet />
-    </Suspense>
+    <AppShell
+      padding="md"
+      header={{ height: 60 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+
+        <div>NetVault</div>
+      </AppShell.Header>
+
+      <AppShell.Navbar>Navbar</AppShell.Navbar>
+
+      <AppShell.Main>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Outlet />
+        </Suspense>
+      </AppShell.Main>
+    </AppShell>
   );
 };
