0be0544f7c815141012057f8541c4fccc6cb31c8
Oleksandr <oleks.bodnar@gmail.com>
2025-12-10 14:52:03 +0200

    added avatar.module.css

--- src/components/RootLayout/AvatarMenu/AvatarMenu.tsx
+++ src/components/RootLayout/AvatarMenu/AvatarMenu.tsx
@@ -6,6 +6,7 @@ import {
   IconChevronUp,
   IconLogout,
 } from "@tabler/icons-react";
+import classes from "./AvatarMenu.module.css";
 
 export const AvatarMenu = () => {
   const [menuOpened, setMenuOpened] = useState(false);
@@ -23,50 +24,15 @@ export const AvatarMenu = () => {
       position="bottom-end"
       opened={menuOpened}
       onChange={setMenuOpened}
-      styles={(theme) => ({
-        dropdown: {
-          backgroundColor: theme.colors.dark[8],
-          border: `1px solid ${theme.colors.dark[6]}`,
-        },
-        item: {
-          color: theme.colors.gray[3],
-          backgroundColor: "transparent",
-          "&:hover": {
-            backgroundColor: "transparent",
-            opacity: 0.4,
-          },
-        },
-      })}
+      classNames={{
+        dropdown: classes.menuDropdown,
+        item: classes.menuItem,
+      }}
     >
       <Menu.Target>
-        <UnstyledButton
-          styles={(theme) => ({
-            root: {
-              display: "flex",
-              alignItems: "center",
-              gap: theme.spacing.xs,
-              padding: `${theme.spacing.xs} ${theme.spacing.sm}`,
-              color: theme.colors.gray[3],
-              borderRadius: theme.radius.md,
-              transition: "all 0.2s",
-              "&:hover": {
-                backgroundColor: theme.colors.dark[7],
-                color: theme.white,
-              },
-            },
-          })}
-        >
-          <Avatar
-            size={28}
-            radius="xl"
-            style={(theme) => ({
-              background: theme.white,
-              color: theme.colors.violet[6],
-              fontSize: theme.fontSizes.xs,
-              fontWeight: 600,
-            })}
-          >
-            AD
+        <UnstyledButton className={classes.button}>
+          <Avatar size={28} radius="xl" className={classes.avatar}>
+            NV
           </Avatar>
           {menuOpened ? (
             <IconChevronUp size={16} />
