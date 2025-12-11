deccbce4c1f1071261ed6c0554024389ec21abc6
Yurii Filimonchuk <filimon4uck.yura@gmail.com>
2025-12-11 18:28:42 +0100

    started test table

---
+++ src/shared/constants/constants.ts
@@ -0,0 +1,12 @@
+export const deviceTableHeaderMap: Record<string, string> = {
+  Name: "name",
+  "IP Address": "ip_address",
+  Port: "port",
+  "Device Type": "device_type_id",
+  Tags: "tags",
+  "Backup Schedule": "backup_schedule",
+  "Last Backup At": "last_backup_at",
+  "Last Backup Status": "last_backup_status",
+  Active: "is_active",
+  "Created At": "created_at",
+};
