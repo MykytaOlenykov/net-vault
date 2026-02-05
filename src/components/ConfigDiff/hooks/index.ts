2ab068c0cb690708c007a3fb39e59f9d2ef6816a
Oleksandr <oleks.bodnar@gmail.com>
2026-02-05 11:38:51 +0200

    add second device dropdown and api connection

---
+++ src/components/ConfigDiff/hooks/index.ts
@@ -0,0 +1,3 @@
+export { useGetDevicesWithConfigs } from "./useGetDevicesWithConfigs";
+export { useGetDeviceConfigs } from "./useGetDeviceConfigs";
+export { useCompareConfigs } from "./useCompareConfigs";
